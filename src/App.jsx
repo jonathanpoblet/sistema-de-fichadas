bc4ee82925d94d3350ec29fd8d891e563b7931c7
unknown <jonathanpoblet228@gmail.com>
2024-06-03 17:03:05 -0300

    Geolocation Added

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,12 @@
-import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap.bundle.min';
-import 'bootstrap';
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min";
+import "bootstrap";
 
-import AppRoute from './router/AppRoute';
-
+import AppRoute from "./router/AppRoute";
 
 function App() {
-  return (
-    <AppRoute />
-  )
+  return <AppRoute />;
 }
 
 export default App;
--- src/pages/PunchIn/PunchIn.jsx
+++ src/pages/PunchIn/PunchIn.jsx
@@ -1,11 +1,70 @@
 import React from "react";
 import PageHeader from "../../components/PageHeader/PageHeader";
 
+import Swal from "sweetalert2";
+
 import "./punchIn.css";
+
+const REFERENCE_LAT = -34.6246251543433;
+const REFERENCE_LNG = -58.45243991674626;
 
 export default function PunchIn() {
   function postPunchIn() {
-    console.log("hola");
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(
+        position => {
+          const { latitude, longitude } = position.coords;
+          if (isWithinRadius(latitude, longitude, REFERENCE_LAT, REFERENCE_LNG, 1)) {
+            Swal.fire({
+              icon: "success",
+              title: "Fichada Enviada",
+              text: "Has enviado con éxito tu fichada",
+              confirmButtonText: "Continuar",
+            });
+          } else {
+            Swal.fire({
+              icon: "error",
+              title: "Error",
+              text: "No te encuentras dentro del rango de tu domicilio de trabajo",
+              confirmButtonText: "Cancelar",
+            });
+          }
+        },
+        error => {
+          Swal.fire({
+            icon: "error",
+            title: "Error",
+            text: "Error del servidor al realizar la fichada",
+            confirmButtonText: "Cancelar",
+          });
+        }
+      );
+    } else {
+      Swal.fire({
+        icon: "error",
+        title: "Error",
+        text: "La geolocalización no es soportada por el navegador",
+        confirmButtonText: "Cancelar",
+      });
+    }
+  }
+
+  function getDistanceFromLatLonInKm(lat1, lon1, lat2, lon2) {
+    const R = 6371;
+    const dLat = deg2rad(lat2 - lat1);
+    const dLon = deg2rad(lon2 - lon1);
+    const a = Math.sin(dLat / 2) * Math.sin(dLat / 2) + Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) * Math.sin(dLon / 2) * Math.sin(dLon / 2);
+    const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+    return R * c;
+  }
+
+  function deg2rad(deg) {
+    return deg * (Math.PI / 180);
+  }
+
+  function isWithinRadius(lat1, lon1, lat2, lon2, radius) {
+    const distance = getDistanceFromLatLonInKm(lat1, lon1, lat2, lon2);
+    return distance <= radius;
   }
 
   return (
